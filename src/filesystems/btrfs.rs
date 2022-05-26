966313fa2d728e3a332dd8872be5f0f705f20ffd
Jeff Rescignano <jeff@jeffresc.dev>
2022-05-25 23:32:17 -0400

    Update Filesystem trait, Subvolume struct, Snapshot struct

--- src/filesystems/btrfs.rs
+++ src/filesystems/btrfs.rs
@@ -3,10 +3,31 @@ use crate::filesystem;
 pub struct BTRFS {}
 
 impl filesystem::Filesystem for BTRFS {
-    fn create_snapshot(&self, subvolume: &String) -> (bool, String, String) {
-        (true, String::from(""), String::from(""))
+    fn create_snapshot(&self, subvolume: &filesystem::Subvolume) -> filesystem::Snapshot {
+        filesystem::Snapshot {
+            name: String::from(""),
+            used_space: 0,
+            mountpoint: String::from("")
+        }
     }
-    fn delete_snapshot(&self, subvolume: &String, snapshot_name: &String) -> bool {
-        true
+
+    fn delete_snapshot(&self, snapshot: &filesystem::Snapshot) -> () {
+
+    }
+
+    fn list_snapshots(&self) -> Vec<filesystem::Snapshot> {
+        vec![filesystem::Snapshot {
+            name: String::from(""),
+            used_space: 0,
+            mountpoint: String::from("")
+        }]
+    }
+
+    fn list_subvolumes(&self) -> Vec<filesystem::Subvolume> {
+        vec![filesystem::Subvolume {
+            name: String::from(""),
+            mountpoint: String::from(""),
+            used_space: 0,
+        }]
     }
 }
--- src/filesystems/filesystem.rs
+++ src/filesystems/filesystem.rs
@@ -1,4 +1,18 @@
 pub trait Filesystem {
-    fn create_snapshot(&self, subvolume: &String) -> (bool, String, String);
-    fn delete_snapshot(&self, subvolume: &String, snapshot_name: &String) -> bool;
+    fn list_subvolumes(&self) -> Vec<Subvolume>;
+    fn list_snapshots(&self) -> Vec<Snapshot>;
+    fn create_snapshot(&self, subvolume: &Subvolume) -> Snapshot;
+    fn delete_snapshot(&self, snapshot: &Snapshot) -> ();
+}
+
+pub struct Subvolume {
+    pub name: String,
+    pub used_space: u64,
+    pub mountpoint: String
+}
+
+pub struct Snapshot {
+    pub name: String,
+    pub used_space: u64,
+    pub mountpoint: String
 }
